4ecc655651bba7b956d89ad5aada81151e33d9ef
Tiffany Ford <me@tiffanyford.dev>
2025-06-18 10:49:27 -0500

    Add project files.

---
+++ dotnet-demo-pipeline/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_demo_pipeline
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
